2d4260e9a5ed20c60c663dcb6a9f48e75c750672
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-01-25 14:38:20 +0100

    component set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-
+import { useState, useEffect } from 'react';
 import './style.css';
+import Die from "./Die"
+
 
 function App() {
+  const [dice, setDice] = useState(allNewDice())
+  function allNewDice() {
+    // new array to hold my numbers
+    const newDice = []
+
+    //loop through them 10 times
+    for (let i = 0; i < 10; i++) {
+      // push a random number from 1-6 to my array
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+  }
+
+  function rollDice(){
+    setDice(allNewDice())
+  }
+
+  const diceElements = dice.map(die => <Die value={die} />)
+
   return (
-    <div className="App">
-     
-    </div>
+    <main>
+      <div className="container">
+        {diceElements}
+      </div>
+      <button className='roll-button' onClick={rollDice}>Roll</button>
+    </main>
   );
 }
 
